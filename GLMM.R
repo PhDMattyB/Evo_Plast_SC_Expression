331ee609f833d11b4276c2d2c9fca6865d8762bc
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-04-23 12:14:14 +0100

    convert seurat to nebula

--- GLMM.R
+++ GLMM.R
@@ -2,16 +2,15 @@ setwd('~/Parsons_Postdoc/NCC_NEOF_Project/Trailmaker/')
 
 library(Seurat)
 library(tidyverse)
-
-sc_data = readRDS(file = 'SSC_Seurat_obj_processed_matrix.rds')
+library(nebula)
 
 sc_data = readRDS(file = 'Evo_Plast_sc_data.rds')
 
 sc_data@misc
 sc_data@meta.data
-sc_data$
 
-nebula_data = scToNeb(obj = sc_data, 
+
+nebula_data <- scToNeb(obj = sc_data, 
                       assay = 'RNA', 
                       id = 'samples', 
                       pred = c('Temp', 
@@ -20,3 +19,7 @@ nebula_data = scToNeb(obj = sc_data,
                                'seurat_clusters', 
                                'cells_id'), 
                       offset = 'nCount_RNA')
+ 
+# df = model.matrix(~seurat_clusters+cells_id, 
+#                   data = nebula_data$pred)
+

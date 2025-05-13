2245234a4af163ffd274d83337cb9a5cdf4690e3
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-05-13 09:06:25 +0100

    Update GLMM.R

--- GLMM.R
+++ GLMM.R
@@ -9,8 +9,24 @@ sc_data = readRDS(file = 'Evo_Plast_sc_data.rds')
 sc_data@misc
 sc_data@meta.data
 
+umap = DimPlot(sc_data, 
+               reduction = 'umap', 
+               label = T)
 
-nebula_data <- scToNeb(obj = sc_data, 
+clust_markers = FindAllMarkers(sc_data, 
+                               only.pos = T)
+
+clust_markers %>%
+  rownames_to_column() %>% 
+  as_tibble() %>% 
+  filter(cluster == 'Cluster 21')%>%
+  filter(p_val_adj <= 0.05) %>%
+  arrange(desc(avg_log2FC)) %>% 
+  select(gene) %>%
+  slice(1:1000) %>%
+  write_tsv('Cluster21_T1000_Marker_genes.txt')
+
+ nebula_data <- scToNeb(obj = sc_data, 
                       assay = 'RNA', 
                       id = 'samples', 
                       pred = c('Temp', 
